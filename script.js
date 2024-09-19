11bfd9b839e4d78232174ad900a418becd154a1c
LaytonDLL <157483774+LaytonDLL@users.noreply.github.com>
2024-09-18 22:46:58 -0300

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait until the page is fully loaded
+window.addEventListener('load', () => {
+    // Add the 'loaded' class to the body to hide the loader and show the content
+    document.body.classList.add('loaded');
+});
